6afea218822a2eca779e0d7bf02e145660aa40a1
chanmi925 <miho.h.ohim@gmail.com>
2023-11-15 17:42:06 +0900

    Homepage.js link fix

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+        <div className='logo'>
+          <h3>ポートフォリオ</h3>
+        </div> 
+    
+
+    <nav>
+      <ul>
+        <li>
+        <Link to='/'>ホーム</Link>
+        </li>
+        <li>
+        <Link to='/blog'>ブログ</Link>
+        </li>
+        <li>
+          <a href='https://github.com/chanmi925'>Github</a>
+        </li>
+      </ul>
+    </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/HomePage.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+import reactImage from "../Images/react.png";
+import jsImage from "../Images/js.png";
+import firebaseImage from "../Images/firebase.jpg";
+import profileImage from "../Images/sibainu.jpg";
+
+function HomePege() {
+  return (
+    <div className="container text-center">
+      {/* <span class="fa-stack fa-4x">
+      <i class="fa-solid fa-address-card"></i> */}
+      {/* </span> */}
+    {/* <h1></h1> */}
+
+    
+
+    <p>
+      Chanmiのポートフォリオサイトです。
+      成果物や身につけたスキルをまとめました。
+    </p>
+
+    <section class="page-section" id="services">
+      <div className="service">
+        <div class="text-center">
+          <h2 class="section-heading text-uppercase">PORTFOLIO</h2>
+          <h3 class="section-subheading text-muted mb-5">
+            私が作った作品一覧です
+          </h3>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa-solid fa-laptop-file"></i>
+            </span>
+            <h4 class="my-3">ポートフォリオサイト</h4>
+            <p class="text-muted">
+              tutorialを見ながらこのポートフォリオサイトを作成しました。
+              もっと手を加えて良いwebサイトにしたいです。
+            </p>
+            <p>使用言語など:HTML/CSS,JavaScript,React,Git</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+            <a href='https://tanslation-app-python.onrender.com/tr/'>
+              <i class="fa-solid fa-language"></i>
+              </a>
+            </span>
+            <h4 class="my-3">翻訳アプリケーション</h4>
+            <p class="text-muted">
+            tutorialを見ながらDjango,DeepLAPIを使用し翻訳アプリケーションを作りました。
+            </p>
+            <p>使用言語など:Django,HTML/CSS,Bootstrap,sqlite,Docker,Dockercompose,render</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="skill">
+      <div class="text-center">
+        {/* <h1 class="title">スキル</h1> */}
+        <div class="row text-center">
+          <div class="col-md-4 services">
+            {/* <img src={reactImage} /> */}
+            {/* <h4>Pyhton</h4>
+            <p>学習中です
+            </p> */}
+          </div>
+          <div class="col-md-4 services">
+            {/* <img src={jsImage} /> */}
+            {/* <h4>HTML/CSS</h4>
+            <p>HTML/CSS,JSの基礎がわかります</p> */}
+          </div>
+          <div class="col-md-4 services">
+            {/* <img src={firebaseImage} /> */}
+            {/* <h4>Django</h4>
+            <p>Djangoがつかえます</p> */}
+          </div>
+        </div>
+        {/* <button type="button" class="btn btn-primary">
+          スキル一覧
+        </button> */}
+      </div>
+    </section>
+  </div>
+  )
+}
+
+export default HomePege
